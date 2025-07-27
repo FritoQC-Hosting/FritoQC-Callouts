7353e362c8667f511cb29ae88d8aed391ac67f44
Michael21107 <121039033+Michael21107@users.noreply.github.com>
2025-07-27 06:43:28 +0200

    Update AssemblyInfo.cs

--- Felix Callouts/Properties/AssemblyInfo.cs
+++ Felix Callouts/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Felix Callouts")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FritoQC Callouts")]
+[assembly: AssemblyDescription("The 2025 version of Felix's Callouts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Felix Callouts")]
+[assembly: AssemblyProduct("FritoQC Callouts")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
